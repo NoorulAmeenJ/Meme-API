d51f2072eb9e502d511aa2e0dbcd83b1c1aa0d7e
NoorulAmeenJ <noorularafat786@gmail.com>
2023-01-12 19:17:25 +0530

    committed

---
+++ script.js
@@ -0,0 +1,14 @@
+// var API= "https://api.imgflip.com/get_memes"
+// fetch(API)
+// .then((response) => response.json())
+// .then((data) =>{
+//    document.body.innerHTML += data.data.memes[0].url
+//     // for(i=0;i<data.data.memes.length;i++)
+// })
+// //    })
+
+
+    // https://api.imgflip.com/get_memes
+// console.log(data.data.memes[0])
+// data.data.memes[0].url
+// data.data.memes[0].name
